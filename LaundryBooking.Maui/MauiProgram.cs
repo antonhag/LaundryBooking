46ba31d76abcf8dc676266392de71ad55b404f5b
antonhag <anton.hagstrom@live.com>
2026-02-26 15:44:27 +0100

    Setup dependency injection and connection string in MauiProgram

--- LaundryBooking.Maui/MauiProgram.cs
+++ LaundryBooking.Maui/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using LaundryBooking.Core.Interfaces;
+using LaundryBooking.Core.Repositories;
+using LaundryBooking.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace LaundryBooking.Maui;
 
@@ -14,6 +18,23 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        // Hämta connection strängen
+        var assembly = typeof(MauiProgram).Assembly;
+        using var stream = assembly.GetManifestResourceStream("LaundryBooking.Maui.appsettings.json"); // öppnar appsettings.json filen där connection strängen finns
+        var config = new ConfigurationBuilder().AddJsonStream(stream!).Build();
+        var connectionString = config["MongoDb:ConnectionString"]!;
+
+        // Registrera repositories
+        builder.Services.AddSingleton<IBookingRepository>(new MongoBookingRepository(connectionString)); // AddSingleton betyder att den skapar en instans och använder den konstant sedan
+        builder.Services.AddSingleton<IIssueRepository>(new MongoIssueRepository(connectionString));
+        builder.Services.AddSingleton<IHousingCooperativeRepository>(new MongoHousingCooperativeRepository(connectionString));
+
+        // Registrera services
+        builder.Services.AddSingleton<BookingService>(); // MAUI skapar BookingService automatiskt och injicerar IBookingRepository som redan är registrerad ovan.
+        builder.Services.AddSingleton<IssueService>();
+        builder.Services.AddSingleton<HousingCooperativeService>();
+        builder.Services.AddSingleton<SessionService>(); // Även här AddSingleton för att använda samma session genom alla olika pages
 
 #if DEBUG
         builder.Logging.AddDebug();
